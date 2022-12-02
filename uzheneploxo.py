16e99e95876746c6629c6dda349ac004c9e28fe0
brut322 <taranvladic.ua@gmail.com>
2022-12-03 00:56:53 +0200

    Fixed: all

---
+++ uzheneploxo.py
@@ -0,0 +1,20 @@
+import pytz
+import requests
+from datetime import datetime, timedelta
+
+
+def get_grapth_data(start_date, days, currency):
+    url = "https://api.exchangerate.host/timeseries?base=USD&start_date={}&end_date={}&symbols={}".format(
+        start_date - timedelta(days=days),
+        start_date,
+        currency
+    )
+
+    print(url)
+
+    response = requests.get(url)
+    data = response.json().get("rates")
+    return [data[key].get(currency, "-") for key in data.keys()]
+
+
+print(get_grapth_data(datetime.now(pytz.timezone("Etc/GMT+1")), 5, "UAH"))
